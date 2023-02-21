58f48646990f90dfad0cd72cdd778d6b72eb8ea0
Leslie Lu <leslie870417@gmail.com>
2023-02-21 00:24:22 -0500

    Milestone 5. Built-in command: cd

--- lab2nyush/nyush.c
+++ lab2nyush/nyush.c
@@ -10,6 +10,16 @@
 #include <unistd.h> //getcwd()
 #include <string.h> //strrchr()
 #include <sys/wait.h>
+#include <signal.h>
+
+static volatile int keepRunning = 0;
+
+void intHandler(//int dummy
+                ) {
+    keepRunning = 1;
+    //exit(0);
+}
+
 
 //Milestone#1: print prompt
 void print_prompt(){
@@ -23,7 +33,10 @@ void print_prompt(){
         ptr = strrchr( cwd, '/' );
         if(ptr == cwd)
          //printf("[nyush %s]$ ", "/");
-          printf("[nyush %s]$ ", ptr);
+//        {  printf("[cwd]%s$ ",cwd);
+            printf("[nyush %s]$ ", ptr);
+            
+//        }
         else
             printf("[nyush %s]$ ", ptr+1);
         fflush(stdout);
@@ -31,7 +44,8 @@ void print_prompt(){
 }
 
 //Milestone#2: repeat prompt, get user input
-void get_user_input(int * exit_c, char ** command){
+void get_user_input( char ** command){
+    //void get_user_input(int * exit_c, char ** command){
     //char * lineptr = NULL;
     size_t n = 0;
 
@@ -46,6 +60,40 @@ void get_user_input(int * exit_c, char ** command){
 //    }
 }
 
+//built-in commands:cd
+void cd(char ** command){
+    
+
+    
+    char *command2 = malloc(100);
+    
+    size_t len_path = strlen(*command);
+    strncpy(command2,*command,len_path -1);
+    
+    //printf("strlen: %zu",len_path);
+//    printf("cdpath: %s",(*command)+3);
+//    printf("cdpath: %s",(*command)+4);
+//    printf("cdpath: %s",(*command)+5);
+//    printf("cdpath: %s",(*command)+6);
+//    printf("cdpath: %s",(*command)+7);
+//    printf("cdpath: %s",(*command)+8);
+//
+//    printf("token1: %s",(token)+2);
+//    printf("token2: %s",(token)+3);
+//    printf("token3: %s",(token)+4);
+//    printf("token4: %s",(token)+5);
+//    printf("token5: %s",(token)+6);
+    //printf("token: %s",(*token)+7);
+    
+    if(chdir((command2)+3) == -1)
+        perror("chdir() error");
+    
+    free(command2);
+    
+    
+}
+
+
 //body
 int main(
         //int argc, const char * argv[]
@@ -53,32 +101,48 @@ int main(
     // insert code here...
     
     //set exit code to default 0
-    int * exit_c = malloc(sizeof(int));
-    *exit_c = 0;
+//    int * exit_c = malloc(sizeof(int));
+//    *exit_c = 0;
+    
+    char ** command = malloc(sizeof(char*));
+    char * concat = malloc(1000);
+    char *str = malloc(1000);
+    char * concat2 = malloc(1000);
+    char *str2 = malloc(1000);
     
     int ret = 0;
     
+    //sighandler_t
+    signal(SIGINT, intHandler);
+    
     //repeat prompt while exit code is 0
-    while(*exit_c == 0){
-        
+    while (1) {
+    //while(*exit_c == 0){
+    //while(TRUE){
         //char * command[1000];
-        char ** command = malloc(sizeof(char*));
+        //char ** command = malloc(sizeof(char*));
 
         print_prompt();
-        get_user_input(exit_c, command);
+        get_user_input(command);
+        //get_user_input(exit_c, command);
+        
+        if(**command == 'c' && *((*command)+1) == 'd'){
+            cd( command);
+            continue;
+        }
         
        // printf("BEFORE:  string:%s\n",  *command);
-        int len = strlen(*command);
+        size_t len = strlen(*command);
         //char * concat = malloc(5+(sizeof(char))*len);
-        char * concat = malloc(1000);
+        //char * concat = malloc(1000);
         strcat(concat, "/bin/");
         
 //        printf("CONCAT:  %s\n",  concat);
 //        printf("LEN_CONCAT:  %ld\n",  strlen(concat));
-        int len_concat = strlen(concat);
+        size_t len_concat = strlen(concat);
 
         //char *str = malloc(sizeof(char)*len);
-        char *str = malloc(1000);
+        //char *str = malloc(1000);
         char *delim = " ,\n";
         char *token;
         strcpy(str, *command);
@@ -87,7 +151,7 @@ int main(
         strcat(concat, token);// "/bin/" + the first arg in command
         
         //char * concat2 = malloc(5+(sizeof(char))*len);
-        char * concat2 = malloc(1000);
+//        char * concat2 = malloc(1000);
         strncpy(concat2,concat,len_concat + len-1); //eliminate '/0'
         
         //printf("AFTER:  string:%s\n",  *command);
@@ -101,7 +165,7 @@ int main(
         }
         
         //char *str2 = malloc(sizeof(char)*len);
-        char *str2 = malloc(1000);
+//        char *str2 = malloc(1000);
         char *delim2 = " ,\n";
         char *token2;
         strcpy(str2, *command);
@@ -117,7 +181,7 @@ int main(
         arglist[arg_num] = NULL;
 
 //        printf("concat2:  %s\n",  concat2);
-//
+////
 //        printf("1MAIN:  string:%s\n",  *arglist);
 //        printf("2MAIN:  string:%s\n",  *(arglist+1));
 //        printf("3MAIN:  string:%s\n",  *(arglist+2));
@@ -159,15 +223,44 @@ int main(
          // waitpid(-1, ...);
         }
         
-        if(*exit_c == -1){
-            free(command);
-            free(concat);
-            free(concat2);
-            free(str);
-            free(str2);
-        }
+
+        
+        * command = malloc(sizeof(char*));
+       concat = malloc(1000);
+      str = malloc(1000);
+         concat2 = malloc(1000);
+        str2 = malloc(1000);
+      //
+        
+//        * command = "";
+//         concat = "";
+//        str = "";
+//         concat2 = "";
+//        str2 = "";
+        
+//        if(keepRunning){
+//            free(*command);
+//            free(command);
+//            free(concat);
+//            free(concat2);
+//            free(str);
+//            free(str2);
+//            //exit(0);
+//            break;
+//        }
+        
     }
     
-    free(exit_c);
+
+   // return EXIT_SUCCESS;
+
+//                free(command);
+//                free(concat);
+//                free(concat2);
+//                free(str);
+//                free(str2);
+//////
+//    exit(0);
+    //free(exit_c);
     return 0;
 }
